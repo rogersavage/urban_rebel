a133a8a71c8532687aff1b8a44c5d6664bbf41fd
Aaron Savage <blain.aaron@gmail.com>
2024-08-30 14:23:27 -0400

    Set up secret key

--- urban_rebel/urban_rebel/settings.py
+++ urban_rebel/urban_rebel/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bt!(6=ec@vmxd(pjen7=qesobc*b@jpd=&9h)e3sp1@1el4^7l'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
